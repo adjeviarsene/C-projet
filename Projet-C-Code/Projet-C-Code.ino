05c9676e70919c899e7541e7b461ad139e1750e7
adjeviarsene <50321446+adjeviarsene@users.noreply.github.com>
2020-05-17 19:52:42 +0300

    Add files via upload
    Fingerprint Sensor Code. C# windows form System  with fingerprint connection.

---
+++ Projet-C-Code/Projet-C-Code.ino
@@ -0,0 +1,51 @@
+
+
+#include <Adafruit_Fingerprint.h>
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+// uncomment this line:
+// #define mySerial Serial1
+
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// comment these two lines if using hardware serial
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+  delay(50);            //don't ned to run this at full speed.
+}
+
+
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  
+}
